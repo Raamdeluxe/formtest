8c6120523fa647a14ad46cd8c878ddefda695042
Raamdeluxe <robin@raamdeluxe.nl>
2023-05-24 23:37:26 +0200

    update 8.0

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const path = require("path");
 
 module.exports = {
 	mode: "development",
-	entry: "./src/form.js", // Assuming your main JavaScript file is named 'index.js' and is located in the 'src' directory.
+	entry: "./src/form.js",
 	output: {
 		filename: "bundle.js",
 		path: path.resolve(__dirname, "dist"),
+		publicPath: "/",
 	},
 };
