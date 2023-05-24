9135b05d5af33864e611037b08993adb87395dfc
Raamdeluxe <robin@raamdeluxe.nl>
2023-05-24 21:59:17 +0200

    second commit

---
+++ src/form.js
@@ -0,0 +1,27 @@
+// create a form element
+const form = document.createElement("form");
+
+// create a input element for name
+const nameInput = document.createElement("input");
+nameInput.setAttribute("type", "text");
+nameInput.setAttribute("name", "name");
+nameInput.setAttribute("placeholder", "Enter your name");
+
+// create a input element for email
+const emailInput = document.createElement("input");
+emailInput.setAttribute("type", "email");
+emailInput.setAttribute("name", "email");
+emailInput.setAttribute("placeholder", "Enter your email");
+
+// create a submit button
+const submitBtn = document.createElement("button");
+submitBtn.setAttribute("type", "submit");
+submitBtn.innerText = "Submit";
+
+// append all the elements to the form
+form.appendChild(nameInput);
+form.appendChild(emailInput);
+form.appendChild(submitBtn);
+
+// append the form to the body
+document.body.appendChild(form);
